02ce77be3eeac19618fd294a658e160015f7bd2a
Beau Nguyen <51268749+beau-nguyen@users.noreply.github.com>
2019-12-23 20:22:23 -0600

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -57,6 +57,7 @@ join departments
 on dep_emp.dept_no= departments.dept_no
 where departments.dept_name= 'Sales' or departments.dept_name= 'Development';
 
+
 --8.  In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 select last_name, count(emp_no)
 from employees
